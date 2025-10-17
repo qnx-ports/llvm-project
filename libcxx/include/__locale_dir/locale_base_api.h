dfb004d61931b37abfed4ce5ea8a965396fd58f1
Felix Xing <fxing@qnx.com>
2025-10-17 11:00:42 -0400

    qnx/libcxx: add qnx os target support

--- libcxx/include/__locale_dir/locale_base_api.h
+++ libcxx/include/__locale_dir/locale_base_api.h
@@ -108,7 +108,7 @@
 
 #  if defined(__APPLE__)
 #    include <__locale_dir/support/apple.h>
-#  elif defined(__FreeBSD__)
+#  elif defined(__FreeBSD__) || defined(__QNX__)
 #    include <__locale_dir/support/freebsd.h>
 #  elif defined(__NetBSD__)
 #    include <__locale_dir/support/netbsd.h>
